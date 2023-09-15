e893908e9e2d55dfbb7ebe191e94bc4d185308a9
40beta <kiptoo.yegon@strathmore.edu>
2023-09-15 20:12:02 +0300

    task4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 fun main(args: Array<String>) {
+
+/*
     println("What is your name?")
     val name = readln()
-    println("Hello,")
+    println("Hello, $name")
+*/
+    println("Enter a number: ")
+
+    // reads line from the standard input - keyboard
+    val stringInput = readLine()!!
+// converts the string input to Integer
+    var integer:Int = stringInput.toInt()
+    println("You entered: $integer")
 }
